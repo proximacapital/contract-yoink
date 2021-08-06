b05b52ac551c4fcf99dac844c2a8e9e53d5b2df3
Oliver Chalk <oliverchalk245@gmail.com>
2021-08-06 10:47:15 +1000

    App: Use commander to parse args

--- Src/App.ts
+++ Src/App.ts
@@ -1,18 +1,26 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
+import PackageJSON from "@Root/package.json";
+import { program } from "commander";
 import fs from "fs";
-import { EtherscanParser, NETWORKS, TEtherscanCodeResult, TNetwork } from "./EtherscanParser";
+import { EtherscanParser, TEtherscanCodeResult, TNetwork } from "./EtherscanParser";
 
-const USER_ARGS_OFFSET: number = 2;
 
+// Cofigure commander
+program
+    .version(PackageJSON.version)
+    .option("-n --network <string>");
+
+
+// Handle user input
 function LogSuccess(aFileName: string): void
 {
     console.log("Successfully wrote", aFileName);
 }
 
-async function YoinkAndWrite(aNetwork: TNetwork, aAddress: string): Promise<void>
+async function YoinkAndWrite(aNetwork: TNetwork | undefined, aAddress: string): Promise<void>
 {
-const lParser: EtherscanParser = new EtherscanParser(aNetwork);
+    const lParser: EtherscanParser = new EtherscanParser(aNetwork);
     const lAddress: string = aAddress.trim();
     const lResult: TEtherscanCodeResult = await lParser.GetFlattenedSourceCode(lAddress);
     const lFileName: string = `./${lAddress}.sol`;
@@ -23,23 +31,21 @@ const lParser: EtherscanParser = new EtherscanParser(aNetwork);
 
 async function main(): Promise<void>
 {
-    const lConsoleArgs: string[] = process.argv.slice(USER_ARGS_OFFSET);
+    program.parse();
+    // if (lConsoleArgs.length <= 1)
+    // {
+    //     let lValidNetworks: string = "";
+    //     NETWORKS.forEach((aNetwork: TNetwork): void => { lValidNetworks += `${aNetwork} | `; });
+    //
+    //     console.error("Expected network followed by 1 or more contract addresses");
+    //     console.error("Valid network options are: " + lValidNetworks);
+    //
+    //     return;
+    // }
 
-    if (lConsoleArgs.length <= 1)
-    {
-        let lValidNetworks: string = "";
-        NETWORKS.forEach((aNetwork: TNetwork): void => { lValidNetworks += `${aNetwork} | ` });
-
-        console.error("Expected network followed by 1 or more contract addresses");
-        console.error("Valid network options are: " + lValidNetworks)
-
-        return;
-    }
-
-    // TODO: default network to mainnet, allow override via --network
-    const lNetwork: TNetwork = lConsoleArgs.shift() as TNetwork;
-
-    lConsoleArgs.forEach((aAddress: string): Promise<void> => YoinkAndWrite(lNetwork, aAddress));
+    const lAddresses: string[] = program.args;
+    const lNetwork: TNetwork | undefined = program.opts().network;
+    lAddresses.forEach((aAddress: string): Promise<void> => YoinkAndWrite(lNetwork, aAddress));
 }
 
 main();
